bfb82d2db30654dd304c70db0a997cd920b6e8d6
Efrain Diaz Martinez <141279566+Efrain34@users.noreply.github.com>
2024-09-25 18:37:39 -0400

    Add files via upload

---
+++ METRONAVIA COWBOY/enemy.gd
@@ -0,0 +1,79 @@
+extends CharacterBody2D
+
+
+var speed = 35
+var player_chase = false
+var player = null
+
+var health = 100
+var player_inattack_zone = false
+var can_take_damage = true
+
+func _physics_process(delta):
+	deal_with_damage()
+	
+	update_health()
+	
+	
+	if player_chase:
+		position += (player.position - position)/speed
+		
+		$AnimatedSprite2D.play("walk")
+		
+		if(player.position.x - position.x) <0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.play("idle")
+
+
+func _on_detection_area_body_entered(body):
+	player = body
+	player_chase = true
+
+
+func _on_detection_area_body_exited(body):
+	player = null
+	player_chase = false
+	
+	
+func enemy():
+	pass
+	
+
+
+func _on_enemy_hitbox_body_entered(body):
+	if body.has_method("player"):
+		player_inattack_zone = true
+
+
+func _on_enemy_hitbox_body_exited(body):
+	if body.has_method("player"):
+		player_inattack_zone = false
+		
+
+func deal_with_damage():
+	if player_inattack_zone == true:
+		if can_take_damage == true:
+			health = health - 20
+			$take_damage_cooldown.start()
+			can_take_damage = false
+			print("magma health = ", health)
+			if health <= 0:
+				self.queue_free()
+
+
+
+func _on_take_damage_cooldown_timeout():
+	can_take_damage = true
+	
+func update_health():
+	var healthbar = $healthbar
+	
+	healthbar.value = health
+	
+	if health > 100:
+		healthbar.visible = false
+	else:
+		healthbar.visible = true
